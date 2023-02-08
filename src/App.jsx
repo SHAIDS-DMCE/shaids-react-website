77c4844517e085273b1fbbc394cb9cc0d11405ab
Yash Bhangale <68957369+yashbhangale@users.noreply.github.com>
2023-02-08 22:36:49 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import styles from "./style";
+import { Footer, Navbar, Hero, OurTeam, Events, } from "./components";
+
+
+
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Events />
+        <OurTeam />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import OurTeam from "./OurTeam";
+import Events from "./Events";
+
+export {
+  Navbar,
+  Footer,
+  Hero,
+  OurTeam,
+  Events,
+};
